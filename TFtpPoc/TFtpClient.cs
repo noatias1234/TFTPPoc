22066bcd7b7dc803638a8b516a5debf1238cfb41
Bar Noa Atias <noatias1234@gmail.com>
2023-03-15 10:38:28 +0200

    TFTP client write file

--- TFtpPoc/TFtpClient.cs
+++ TFtpPoc/TFtpClient.cs
@@ -1,4 +1,5 @@
 ﻿using System.Net;
+using System.Text;
 using Tftp.Net;
 
 namespace TFtpPoc;
@@ -19,19 +20,76 @@ internal class TFtpClient
 
     public void ReadFile(string fileName)
     {
-        var tftpTransfer = _tftpClient.Download(fileName);
 
         using Stream stream = new MemoryStream();
-        tftpTransfer.Start(stream);
+        var content = string.Empty;
+
+        var transfer = _tftpClient.Download(fileName);
+        transfer.RetryCount = 3;
+        transfer.RetryTimeout= TimeSpan.FromSeconds(2);
+        transfer.TransferMode = TftpTransferMode.octet;
+
+        transfer.OnProgress += (_, progress) => { Console.WriteLine("Transfer running. Progress: " + progress); };
+
+        transfer.OnFinished += (_) =>
+        {
+            Console.WriteLine("Succeed");
+            using var reader = new StreamReader(stream);
+            content = reader.ReadLine();
+            stream.Position = 0;
+            TransferFinishedEvent.Set();
+        };
+
+        transfer.OnError += (_, error) =>
+        {
+            Console.WriteLine("error" + error);
+            stream.Dispose();
+            TransferFinishedEvent.Set();
+        };
+        
+        transfer.Start(stream);
 
         TransferFinishedEvent.WaitOne();
         Console.ReadKey();
     }
-    public void WriteToFile(string fileName)
+    public void WriteToFile(string fileName, string content)
     {
-        var tftpTransfer = _tftpClient.Upload(fileName);
+        try
+        {
+            var byteArray = Encoding.ASCII.GetBytes(content);
+            var stream = new MemoryStream(byteArray);
 
-        Stream stream = new MemoryStream();
-        tftpTransfer.Start(stream);
+            var transfer = _tftpClient.Upload(fileName);
+            transfer.RetryCount = 3;
+            transfer.RetryTimeout = TimeSpan.FromSeconds(1);
+
+            transfer.OnProgress += (_, progress) =>
+            {
+                Console.WriteLine("Transfer running. Progress: " + progress);
+            };
+
+            transfer.OnFinished += _ =>
+            {
+                Console.WriteLine("Transfer succeeded.");
+                stream.Dispose();
+                TransferFinishedEvent.Set();
+            };
+
+            transfer.OnError += (_, error) =>
+            {
+                Console.WriteLine("Transfer failed: " + error);
+                stream.Dispose();
+                TransferFinishedEvent.Set();
+            };
+
+            transfer.Start(stream);
+
+            TransferFinishedEvent.WaitOne();
+        }
+        catch (Exception e)
+        {
+            Console.WriteLine(e);
+            throw;
+        }
     }
 }
